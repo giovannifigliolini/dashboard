d8f62ea44aab096d43d107e7a3208f902dee4036
Giovanni <g.figliolini@almaviva.it>
2025-01-17 15:31:38 +0100

    fix

--- src/app/components/widget/widget.component.ts
+++ src/app/components/widget/widget.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Widget } from '../../models/dashboard';
+import { NgComponentOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-widget',
   standalone: true,
-  imports: [],
+  imports: [NgComponentOutlet],
   templateUrl: './widget.component.html',
   styleUrl: './widget.component.scss'
 })
 export class WidgetComponent {
+
+  //Dichiariamo data come input e il tipo è Widget
+  data = input.required<Widget>()
 
 }
