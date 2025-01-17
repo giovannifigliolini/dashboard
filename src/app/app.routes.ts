de0a60c20e1cb43e576e67adc298d996e7eb2206
Giovanni <g.figliolini@almaviva.it>
2025-01-17 12:23:15 +0100

    rotte

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AnalyticsComponent } from './pages/analytics/analytics.component';
+import { ContentComponent } from './pages/content/content.component';
+import { CommentsComponent } from './pages/comments/comments.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: 'analytics',
+        component: AnalyticsComponent
+    },
+    {
+        path: 'content',
+        component: ContentComponent
+    },
+    {
+        path: 'comments',
+        component: CommentsComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard',
+    }
+];
